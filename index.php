260bef14574094738b2e63a5b50c6d1400ea0f42
Erlandas <30452017+erlandasz@users.noreply.github.com>
2019-03-25 00:44:47 +0200

    bbzKasCiaVyksta

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 
     require_once __DIR__.("/vendor/autoload.php");
 
-    $numbers = array(5, 10, 15);
-    echo calculateHomeWorkSum($numbers);
 
+    $numbers = array(5, 10.1, "15");
+    echo calculateHomeWorkSum($numbers);
+    echo "<hr>";
+
+    use function Nfq\Akademija\Not_Typed\calculateHomeWorkSum;
+        $numbers = array(1, "2.5" , 3);
+        echo calculateHomeWorkSum($numbers);
+    echo "<hr>";
+
+    use Nfq\Akademija\Soft\calculateHomeWorkSum;
+        $numbers = array(11, 11.3, 11, 13.25, 1.25);
+        echo calculateHomeWorkSum($numbers);
+    echo "<hr>"
+    
 ?>
--- src/functions.php
+++ src/functions.php
@@ -7,4 +7,16 @@ namespace {
     }
 
 }
+namespace Nfq\Akademija\Not_Typed {
+    function calculateHomeWorkSum($numbers): int {
+    if(!is_array($numbers)) return false;
+    return array_sum($numbers);
+    }
+}
+namespace Nfq\Akademija\Soft {
+    function calculateHomeWorkSum(int...$numbers): int {
+        if(!is_array($numbers)) return false;
+        return array_sum($numbers);
+    }
+}
 ?>
