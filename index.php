e653c654b11844b18a17a68b0d5aa557b05e5f55
Erlandas <30452017+erlandasz@users.noreply.github.com>
2019-03-25 15:21:48 +0200

    just readme.md left

--- index.php
+++ index.php
@@ -4,17 +4,21 @@
 
 
     $numbers = array(5, 10.1, "15");
-    echo calculateHomeWorkSum($numbers);
+    echo "calculateHomeWorkSum:". calculateHomeWorkSum($numbers);
     echo "<hr>";
 
-    use function Nfq\Akademija\Not_Typed\calculateHomeWorkSum;
+    use function \Nfq\Akademija\Not_Typed\calculateHomeWorkSum;
         $numbers = array(1, "2.5" , 3);
-        echo calculateHomeWorkSum($numbers);
+        echo "Nfq\Akademija\Not_Typed\calculateHomeWorkSum is:". calculateHomeWorkSum($numbers);
     echo "<hr>";
 
-    use Nfq\Akademija\Soft\calculateHomeWorkSum;
+    use \Nfq\Akademija\Soft\calculateHomeWorkSum; 
         $numbers = array(11, 11.3, 11, 13.25, 1.25);
-        echo calculateHomeWorkSum($numbers);
-    echo "<hr>"
+        echo "Nfq\Akademija\Soft\calculateHomeWorkSum is:". calculateHomeWorkSum($numbers);
+    echo "<hr>";
     
+    use \Nfq\Akademija\Strict as Moo;
+        $numbers = array(1, 3.3, "2");
+        echo "Nfq\Akademija\Strict\calculateHomeWorkSum is:". Moo/calculateHomeWorkSum($numbers);
+    echo "<hr>"
 ?>
---
+++ src/functions_strict.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Nfq\Akademija\Strict {
+    function calculateHomeWorkSum(int...$numbers): int {
+        if(!is_array($numbers)) return false;
+        return array_sum($numbers);
+    }
+}
+
+?>
