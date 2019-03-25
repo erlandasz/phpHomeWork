9363ecd0ee214ac1c1eeae41597256d71bf0518c
Erlandas <30452017+erlandasz@users.noreply.github.com>
2019-03-25 17:28:37 +0200

    fixai

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
     require_once __DIR__.("/vendor/autoload.php");
 
-        echo "calculateHomeWorkSum:". calculateHomeWorkSum([10, 10, 10]);
+        echo "calculateHomeWorkSum:". calculateHomeWorkSum(10, "15", 12.5);
     echo "<hr>";
 
-        echo "Nfq\Akademija\Not_Typed\calculateHomeWorkSum is:". Nfq\Akademija\Not_Typed\calculateHomeWorkSum([1, 2.5, 3]);
+        echo "Nfq\Akademija\Not_Typed\calculateHomeWorkSum is:". Nfq\Akademija\Not_Typed\calculateHomeWorkSum(1, 2.5, 3);
     echo "<hr>";
 
-        echo "Nfq\Akademija\Soft\calculateHomeWorkSum is:". Nfq\Akademija\Soft\calculateHomeWorkSum(11, 11.3, 11, 13.25, 1.25);
+        echo "Nfq\Akademija\Soft\calculateHomeWorkSum is:". Nfq\Akademija\Soft\calculateHomeWorkSum(3, 2.2, '1');
     echo "<hr>";
     
-        echo "Nfq\Akademija\Strict\calculateHomeWorkSum is:". Nfq\Akademija\Strict\calculateHomeWorkSum(1, "2.5", 4);
+        echo "Nfq\Akademija\Strict\calculateHomeWorkSum is:". Nfq\Akademija\Strict\calculateHomeWorkSum(3, 2.2, '1');
     echo "<hr>"
 ?>
--- src/functions.php
+++ src/functions.php
@@ -1,20 +1,17 @@
 <?php 
 
 namespace {
-    function calculateHomeWorkSum($numbers) {
-        if (!is_array($numbers)) return false;
+    function calculateHomeWorkSum(...$numbers) {
         return array_sum($numbers);
     }
 }
 namespace Nfq\Akademija\Not_Typed {
-    function calculateHomeWorkSum($numbers): int {
-    if(!is_array($numbers)) return false;
+    function calculateHomeWorkSum(...$numbers): int {
     return array_sum($numbers);
     }
 }
 namespace Nfq\Akademija\Soft {
     function calculateHomeWorkSum(int...$numbers): int {
-        if(!is_array($numbers)) return false;
         return array_sum($numbers);
     }
 }
